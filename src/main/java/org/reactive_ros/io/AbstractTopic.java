fd65b8e0ad32863a71db907a4aeb50c0789c55fd
omelkonian <sdi1000128@di.uoa.gr>
2016-02-25 20:19:27 +0200

    Added AbstractTopic::extract method

--- src/main/java/org/reactive_ros/io/AbstractTopic.java
+++ src/main/java/org/reactive_ros/io/AbstractTopic.java
@@ -1,6 +1,15 @@
 package org.reactive_ros.io;
 
+import org.reactive_ros.Stream;
 import org.reactive_ros.evaluation.Serializer;
+import org.reactive_ros.internal.expressions.creation.FromSource;
+import org.reactive_ros.internal.graph.FlowGraph;
+import org.reactive_ros.internal.output.MultipleOutput;
+import org.reactive_ros.internal.output.Output;
+import org.reactive_ros.internal.output.SinkOutput;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Orestis Melkonian
@@ -25,6 +34,26 @@ public abstract class AbstractTopic<T, B> implements Source<T>, Sink<T> {
         return name + "[" + Thread.currentThread().getId() + "]";
     }
 
+    public static List<AbstractTopic> extract(Stream stream, Output output) {
+        List<AbstractTopic> topics = stream.getGraph()
+                .vertexSet()
+                .stream()
+                .filter(n -> n instanceof FromSource)
+                .map(n -> ((FromSource) n).getSource())
+                .filter(s -> s instanceof AbstractTopic)
+                .map(s -> ((AbstractTopic) s))
+                .collect(Collectors.toList());
+
+        if (output instanceof MultipleOutput) {
+            for (Output out : ((MultipleOutput) output).getOutputs())
+                if (out instanceof SinkOutput && ((SinkOutput) out).getSink() instanceof AbstractTopic)
+                    topics.add((AbstractTopic) ((SinkOutput) out).getSink());
+        }
+        else if (output instanceof SinkOutput && ((SinkOutput) output).getSink() instanceof AbstractTopic)
+            topics.add((AbstractTopic) ((SinkOutput) output).getSink());
+
+        return topics;
+    }
 
     @Override
     public String toString() {
