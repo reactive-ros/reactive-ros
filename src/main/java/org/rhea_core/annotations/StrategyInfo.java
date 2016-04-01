b9d79e664e41e224d8934baa5fb451a59965d5c0
omelkonian <sdi1000128@di.uoa.gr>
2016-04-01 21:11:50 +0300

    Added name property to StrategtyInfo

--- src/main/java/org/rhea_core/annotations/StrategyInfo.java
+++ src/main/java/org/rhea_core/annotations/StrategyInfo.java
@@ -11,6 +11,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface StrategyInfo {
+    String name();
     String[] requiredSkills() default "";
     int priority() default 0;
 }
