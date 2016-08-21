3b5269ef88409565434fe59ca0cd980b1afc2ba3
Orestis Melkonian <melkon.or@gmail.com>
2016-08-21 20:46:44 +0200

    Added split graph-construction test

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -2,7 +2,13 @@ import graph_viz.GraphVisualizer;
 import org.junit.Test;
 import org.junit.experimental.theories.suppliers.TestedOn;
 import org.rhea_core.Stream;
+import org.rhea_core.internal.expressions.Transformer;
+import org.rhea_core.internal.expressions.combining.MergeExpr;
+import org.rhea_core.internal.expressions.combining.MergeMultiExpr;
 import org.rhea_core.internal.expressions.combining.ZipExpr;
+import org.rhea_core.internal.expressions.creation.FromExpr;
+import org.rhea_core.internal.expressions.transformational.MapExpr;
+import org.rhea_core.internal.graph.FlowGraph;
 import org.rhea_core.optimization.DefaultOptimizationStrategy;
 import org.rhea_core.optimization.OptimizationStrategy;
 import org.rhea_core.optimization.optimizers.NodeMerger;
@@ -16,12 +22,60 @@ import test_data.utilities.Threads;
 
 import java.io.*;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
  * @author Orestis Melkonian
  */
 public class Tester {
+
+    @Test
+    public void split() {
+        Stream<Integer> s0 = Stream.just(0, 1, 2);
+
+        Stream<Integer> s1 = s0.map(i -> i + 1);
+        Stream<Integer> s2 = s0.map(i -> i + 2);
+        Stream<Integer> s3 = s0.map(i -> i + 3);
+
+        Stream<Integer> s = Stream.merge(s1, s2, s3);
+
+
+        FlowGraph g = s.getGraph();
+        List<Transformer> roots = g.getRoots();
+        assert roots.size() == 1;
+        Transformer root = roots.get(0);
+        assert root instanceof FromExpr;
+
+        List<Transformer> successors = g.successors(root);
+        assert successors.size() == 3;
+
+        Transformer map1 = successors.get(0);
+        assert map1 instanceof MapExpr;
+        Transformer map2 = successors.get(1);
+        assert map2 instanceof MapExpr;
+        Transformer map3 = successors.get(2);
+        assert map3 instanceof MapExpr;
+
+        assert g.successors(map1).size() == 1;
+        assert g.successors(map2).size() == 1;
+        assert g.successors(map3).size() == 1;
+
+        Transformer m1 = g.successor(map1);
+        assert m1 instanceof MergeMultiExpr;
+        Transformer m2 = g.successor(map2);
+        assert m2 instanceof MergeMultiExpr;
+        Transformer m3 = g.successor(map3);
+        assert m3 instanceof MergeMultiExpr;
+
+        assert m1 == m2;
+        assert m2 == m3;
+
+        assert m1 == g.getConnectNode();
+
+        GraphVisualizer.display(g);
+        Threads.sleep();
+    }
 
 //    @Test
     public void serialization() {
@@ -48,7 +102,7 @@ public class Tester {
         return true;
     }
 
-    @Test
+//    @Test
     public void minter() {
         new Thread(() -> System.out.println(IdMinter.next())).run();
         new Thread(() -> System.out.println(IdMinter.next())).run();
